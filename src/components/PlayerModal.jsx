a2692d420290e4fa141b6383cbf20e0c6ff67189
rahulfordev <rahulforsms@gmail.com>
2024-10-05 16:42:39 +0600

    chart added

--- src/components/PlayerModal.jsx
+++ src/components/PlayerModal.jsx
@@ -1,10 +1,82 @@
 /* eslint-disable @next/next/no-img-element */
+import { Bar } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+	Colors,
+} from "chart.js";
+
+// Register components with Chart.js
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
+);
+
 const PlayerModal = ({ player, isOpen, onClose }) => {
 	if (!isOpen || !player) return null;
+	const data = {
+		labels: [
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+		],
+		datasets: [
+			{
+				label: "Ranking",
+				data: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+				backgroundColor: "rgba(189, 128, 0, 0.8)",
+				borderColor: "rgba(189, 128, 0, 1)",
+				borderWidth: 1,
+			},
+		],
+	};
 
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true,
+				reverse: true, // Reversing to have 1 on top
+				ticks: {
+					stepSize: 1,
+				},
+			},
+		},
+		plugins: {
+			title: {
+				display: true,
+				text: "2023-2024 Season Rankings",
+			},
+		},
+	};
 	return (
 		<div className="h-[600px] inset-0 flex items-center justify-center sm:justify-end z-50">
 			<div className="bg-white rounded-lg shadow-xl max-w-sm w-full p-4 relative">
+				<div className="bg-[#a00000] rounded-lg mb-3">
+					<Bar data={data} options={options} />
+				</div>
+
 				<div className="flex gap-x-3 mb-2">
 					<img
 						src={player?.image}
